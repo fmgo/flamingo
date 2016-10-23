3ef35512250c8578dc7d54e92115c4b33f71c5ee
Fmgo <fmgo.trading@gmail.com>
2016-10-23 18:21:48 +0200

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "installedESLint": true,
+  "env": {
+    "node": true,
+    "es6": true,
+    "mocha": true,
+  },
+  "rules": {
+    "no-underscore-dangle": 0,
+    "new-cap": 0,
+  },
+};
